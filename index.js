cd85619f3598092de7e1b05e5c1bdfd0fafb1898
Chris Cardle <cdcardle@gmail.com>
2017-09-13 14:29:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(element) {
-  dakittens = kittens.push(element)
+  kittens.push(element)
 }
 
 function destructivelyPrependKitten(element) {
