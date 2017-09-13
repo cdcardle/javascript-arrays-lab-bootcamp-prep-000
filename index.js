2f02f7a3029fadf5fc115deac13877f6b3fb7f7e
Chris Cardle <cdcardle@gmail.com>
2017-09-13 14:29:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(element) {
+  dakittens = kittens.push(element)
+}
+
+function destructivelyPrependKitten(element) {
+  dpkittens = kittens.unshift(element)
+}
+
+function destructivelyRemoveLastKitten() {
+  drlkittens = kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  drfkittens = kittens.shift()
+}
+
+function appendKitten(element) {
+  akittens = [...kittens, element]
+}
+
+function prependKitten(element) {
+  pkittens = [element, ...kittens]
+}
+
+function removeLastKitten() {
+  rlkittens = kittens.slice(-(kittens.length - 1))
+}
+function removeFirstKitten() {
+  rfkittens = kittens.slice(1)
+}
